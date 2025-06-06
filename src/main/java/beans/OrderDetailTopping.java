dc6994e6ee3b8779756dc5ec0d6a72712538ec8f
Tran Kien Cuong <trankiencuong30072003@gmail.com>
2025-06-06 16:51:13 +0700

    feat: add OrderDetailTopping bean class to represent toppings for order items

---
+++ src/main/java/beans/OrderDetailTopping.java
@@ -0,0 +1,26 @@
+package beans;
+
+public class OrderDetailTopping {
+	private int id;
+	private int chiTietDonHang;
+	private int topping;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getChiTietDonHang() {
+		return chiTietDonHang;
+	}
+	public void setChiTietDonHang(int chiTietDonHang) {
+		this.chiTietDonHang = chiTietDonHang;
+	}
+	public int getTopping() {
+		return topping;
+	}
+	public void setTopping(int topping) {
+		this.topping = topping;
+	}
+	
+}
