384d282fe1c00923d0a7dc1d9181456e1151f164
Tran Kien Cuong <trankiencuong30072003@gmail.com>
2025-06-06 16:49:48 +0700

    feat: add Category bean class to represent product categories

---
+++ src/main/java/beans/Category.java
@@ -0,0 +1,40 @@
+package beans;
+
+public class Category {
+    private int maDM;
+    private String tenDM;
+    private String moTa;
+    private boolean trangThai;
+
+    public int getMaDM() {
+        return maDM;
+    }
+
+    public void setMaDM(int maDM) {
+        this.maDM = maDM;
+    }
+
+    public String getTenDM() {
+        return tenDM;
+    }
+
+    public void setTenDM(String tenDM) {
+        this.tenDM = tenDM;
+    }
+
+    public String getMoTa() {
+        return moTa;
+    }
+
+    public void setMoTa(String moTa) {
+        this.moTa = moTa;
+    }
+
+    public boolean isTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(boolean trangThai) {
+        this.trangThai = trangThai;
+    }
+} 
