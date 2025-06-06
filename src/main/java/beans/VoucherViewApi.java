f6aa9e92345d2271e54831ea30c8cb8c16d1a8e7
Tran Kien Cuong <trankiencuong30072003@gmail.com>
2025-06-06 16:56:19 +0700

    feat: add VoucherViewApi bean class to support voucher display API

---
+++ src/main/java/beans/VoucherViewApi.java
@@ -0,0 +1,12 @@
+package beans;
+
+import java.math.BigDecimal;
+
+public class VoucherViewApi {
+	public int id;
+	public String code;
+	public String name;
+	public String description;
+	public BigDecimal percentDiscount;
+	public BigDecimal valueDiscount;
+}
