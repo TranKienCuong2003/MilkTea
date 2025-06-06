191e39503c04b391adf72b797d2bb9ba25f56c79
Tran Kien Cuong <trankiencuong30072003@gmail.com>
2025-06-06 16:53:12 +0700

    feat: add Size bean class to represent product sizes

---
+++ src/main/java/beans/Size.java
@@ -0,0 +1,30 @@
+package beans;
+
+import java.math.BigDecimal;
+
+public class Size {
+
+	private int maSize;
+	private String tenSize;
+	private BigDecimal heSoGia;
+	public int getMaSize() {
+		return maSize;
+	}
+	public void setMaSize(int maSize) {
+		this.maSize = maSize;
+	}
+	public String getTenSize() {
+		return tenSize;
+	}
+	public void setTenSize(String tenSize) {
+		this.tenSize = tenSize;
+	}
+	public BigDecimal getHeSoGia() {
+		return heSoGia;
+	}
+	public void setHeSoGia(BigDecimal heSoGia) {
+		this.heSoGia = heSoGia;
+	}
+	
+	
+}
